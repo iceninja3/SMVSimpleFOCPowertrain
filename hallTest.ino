fe0743df47b453d28e5003038c014e43308877d1
Vishal Dandamudi <98727663+iceninja3@users.noreply.github.com>
2026-01-15 20:24:26 -0800

    Add hallTest.ino for reading hall sensor states

---
+++ hallTest.ino
@@ -0,0 +1,21 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(115200);
+pinMode(40, INPUT);
+pinMode(41, INPUT);
+pinMode(42, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int hallA = digitalRead(42);
+int hallB = digitalRead(41);
+int hallC = digitalRead(40);
+
+Serial.print("Hall states: ");
+Serial.print(hallA);
+Serial.print(" ");
+Serial.print(hallB);
+Serial.print(" ");
+Serial.println(hallC);
+}
